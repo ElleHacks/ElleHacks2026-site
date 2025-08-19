b897aa466e0514dbb87dd43a77ad1392e0709caa
Joanne Wijetunga <joanne04@my.yorku.ca>
2025-08-18 23:55:08 -0400

    Added base mock-site with placeholders for all sections

---
+++ src/components/MeetTheTeam/MeetTheTeam.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MeetTheTeam.module.css';
+
+const MeetTheTeam = () => {
+    return (
+        <div className={styles.meetTheTeamSection}>
+            <p className={styles.highlight}>
+                Meet the Team
+            </p>                        
+        </div>
+    );
+};
+
+export default MeetTheTeam;
