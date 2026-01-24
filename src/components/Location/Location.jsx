778295bddfe200329930361945dad77c7079444e
Sidney <user@LE-K2I-FJQ33Y.local>
2026-01-24 15:44:41 -0500

    updated location section

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -9,7 +9,7 @@ const Location = () => {
       {/* Google Maps iframe */}
       <iframe
         className={styles.mapIframe}
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2880.971353796361!2d-79.50444332381684!3d43.7734534710966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cd330b767bfb%3A0xdbb899cc9da76d19!2sYork%20University!5e0!3m2!1sen!2sca!4v1755444109058!5m2!1sen!2sca"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.014446736064!2d-79.50196532407767!3d43.772558944703164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2e24bfaaaaab%3A0xd359cc0577e68c34!2sAccolade%20East%20Building!5e0!3m2!1sen!2sca!4v1769287273681!5m2!1sen!2sca"
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
@@ -17,14 +17,14 @@ const Location = () => {
 
       <h2 className={styles.addressTitle}>Address</h2>
       <p className={styles.addressText}>
-        York University,<br />
-        4700 Keele St,<br />
-        North York, ON M3J 1P3
+        Accolade East Building,<br />
+        83 York Blvd,<br />
+        North York, ON M7A 2C5
       </p>
 
       <div className={styles.interactiveMapButton}></div>
       <a
-        href="https://map.concept3d.com/?id=1200#!m/317129?s/?sbc/"
+        href="https://map.concept3d.com/?id=1200#!ce/34557?m/317123?s/"
         className={styles.interactiveMapText}
         target="_blank"
         rel="noopener noreferrer"
